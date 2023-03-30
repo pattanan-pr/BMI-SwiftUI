a5b8d46797b77f8ed2104b866058d3a48c52288c
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2023-03-30 10:40:05 +0700

    Initial Commit

---
+++ NewBMICalculator/NewBMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewBMICalculatorApp.swift
+//  NewBMICalculator
+//
+//  Created by Pattanan Prarom on 30/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct NewBMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
